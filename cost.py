153ee6a4be1979bfdb804963f89ce5040c7c0f3a
Raj Sahu <sahuraj457@gmail.com>
2023-01-08 18:58:24 +0530

    graph added

--- cost.py
+++ cost.py
@@ -11,9 +11,22 @@ def Cost(x,y,w,b):
     cost = cost / m
     return cost
 
-x = np.array([[.5,1.5],[1,1],[1.5,.5],[2,2],[1,2.5]])
+x = np.array([[0.5,1.5],[1,1],[1.5,.5],[2,2],[1,2.5]])
 y = np.array([0,0,0,1,1,1])
-w,b = np.array([1,1]),-4
+w,b = np.array([1,1]),-3
 
 cost = Cost(x,y,w,b)
-print(cost)
+
+x0 = np.arange(0,6)
+x1 = 3 - x0
+x1_other = 4 - x0
+
+plt.plot(x0,x1,c='blue',label='$b=-3$')
+plt.plot(x0,x1_other,c='magenta',label='$b=-4$')
+plt.axis([0,4,0,4])
+plt.scatter(x[:,0],x[:,1])
+plt.xlabel('$x_0$')
+plt.ylabel('$x_1$')
+plt.legend()
+plt.title('Decision Boundary')
+plt.show()
